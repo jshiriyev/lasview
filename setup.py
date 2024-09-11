b9e67495b05dad7a5e07e9930d5f67abb155838c
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2024-09-11 08:05:06 +0400

    remains #2, adding log scale

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'lasview',
-	version = '0.0.52',
+	version = '0.0.53',
 	packages = find_packages(),
 	install_requires = [
 		'numpy>=1.26.4',
